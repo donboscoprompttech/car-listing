39b1c078961c4410dd6e43f12f6914160021326f
sumila <sumila.c@gmail.com>
2022-07-14 15:43:15 +0530

    question issue

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -332,10 +332,11 @@
           <div class="col-lg-8">
             <div class="accordion special-accordion" id="accordionExample">
               <?php $i=0;
+              $i1=1;
 $k=1;
               ?>
               @foreach($questions as $row)
-         
+         <?php if ($i1<=4){?>
               <div class="accordion-item">
                 <h2 class="accordion-header" id="headingOne<?php echo $i?>">
                   <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne<?php echo $i?>"
@@ -373,7 +374,10 @@ else{
                   </div>
                 </div>
               </div>
-              <?php $i++;?>
+              <?php $i++;
+$i1++;
+}
+              ?>
               @endforeach
               
             </div>
